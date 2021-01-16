6aa3d79cc37a31e295050029fcb553ff75e4aaf7
chuoitri0147 <59395771+chuoitri0147@users.noreply.github.com>
2021-01-16 22:02:07 +0700

    https://github.com/Hung271999/webluyenthi.git
    tricommit

--- accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.conf.urls import url
-from .import views
-urlpatterns = [
-     url(r'^login$', views.LoginClass.as_view(), name='login'),
-     url(r'^register$',views.register.as_view(), name='register'),
-     url(r'^changepassword$', views.changepassword.as_view(), name='changepassword'),
-     url(r'^logout$', views.logoutt, name='logout'),
-]  
--- theme/context_processors.py
+++
@@ -1,5 +0,0 @@
-from home.models import Subjects
-def exam(request):
-    list_subjects=Subjects.objects.all()
-    context={"list_subjects":list_subjects}
-    return(context)
